eec88b382b3bbac120320f35bb8106b31e433c7c
Wes Baker <wes@wesbaker.com>
2022-07-06 11:26:52 -0400

    Convert commonjs to esm

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const alfy = require("alfy");
-const pify = require("pify");
-const xml2js = require("xml2js");
+import alfy from "alfy";
+import pify from "pify";
+import xml2js from "xml2js";
 
 const parseString = pify(xml2js.parseString);
 
@@ -8,12 +8,12 @@ alfy
   .fetch("https://www.goodreads.com/search/index.xml", {
     searchParams: { q: alfy.input, key: "YVABuJSFNNFq65uTzRA8Nw" },
     json: false,
-    transform: body =>
+    transform: (body) =>
       parseString(body).then(
-        body => body.GoodreadsResponse.search[0].results[0].work
-      )
+        (body) => body.GoodreadsResponse.search[0].results[0].work
+      ),
   })
-  .then(books => {
+  .then((books) => {
     // No results
     if (books === undefined) {
       return alfy.output([
@@ -22,12 +22,12 @@ alfy
           subtitle: `Search Goodreads for '${alfy.input}'`,
           arg: `https://www.goodreads.com/search?q=${encodeURIComponent(
             alfy.input
-          )}`
-        }
+          )}`,
+        },
       ]);
     }
 
-    const items = books.map(book => {
+    const items = books.map((book) => {
       const { best_book } = book;
       const average_rating =
         typeof book.average_rating[0] === "object"
@@ -49,7 +49,7 @@ alfy
       return {
         title,
         subtitle,
-        arg: `http://goodreads.com/book/show/${id}`
+        arg: `http://goodreads.com/book/show/${id}`,
       };
     });
     alfy.output(items);
